00f86dd76ff90fff75ccdf793a15016294bf5618
jenettosyo <jenettosyo@gmail.com>
2019-11-28 18:26:33 +0900

    create.pull

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,3 +1,9 @@
 $(function(){
+  $(".header__container__right-box__btn").click(function(){
+    $(".header__container__right-box__btn__box").fadeIn();
+  });
 
+  $(".fa").click(function(){
+    $(".header__container__right-box__btn__box").fadeOut();
+  });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'lunch/index'
+
   # get 'homes/index'
 
   root to: "homes#index"
+  resources :lunch, only: [:index]
 end
